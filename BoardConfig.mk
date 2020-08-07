2c48f5c60548030e57046c9cde233915b7d3b0a2
emincangencer <emincange@gmail.com>
2020-08-07 12:36:02 +0200

    V16.0615.2006.131

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@ DEVICE_PATH := device/asus/X00QD
 BOARD_VENDOR := asus
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-02-01
+VENDOR_SECURITY_PATCH := 2020-06-01
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
